51ef79b2152b20e285050e588407bb92f4f4315e
TatyanaSerK <tsk.kalinina@gmail.com>
2024-10-07 19:52:27 +0300

    hw2

--- myapp/task1/models.py
+++ myapp/task1/models.py
@@ -13,7 +13,6 @@ class Game(models.Model):
     title = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=6, decimal_places=3)
     size = models.DecimalField(max_digits=6, decimal_places=3)
-    models.DecimalField(max_digits=6, decimal_places=3)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='game')
